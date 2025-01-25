6f30f99ba23f5c70279492c27e5ab1dd3f2d4af5
muntazimalikhan <muntazimalikhan@globussoft.in>
2025-01-25 23:38:16 +0530

    ++Updated UI Codes++

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,7 +12,7 @@ namespace MeteoraBot
         public MainWindow()
         {
             InitializeComponent();
-            MainGrid.DataContext = MainViewModel.Instance;
+            this.DataContext = MainViewModel.Instance;
         }
 
         private void OnWindowClosed(object sender, EventArgs e)
@@ -22,6 +22,57 @@ namespace MeteoraBot
                 Process.GetCurrentProcess().Kill();
             }
             catch { }
+        }
+
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+            TextBlockModelName.Visibility = Visibility.Collapsed;
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+        }
+
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+            TextBlockModelName.Visibility = Visibility.Visible;
+        }
+
+        private void ListViewMenu_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.Close();
+        }
+
+        private void ToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void ThemeToggle_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ThemeToggle_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
         }
     }
 }
